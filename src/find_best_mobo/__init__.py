35c948434833bf4ec4069ac488f94756aeaba515
GrimsVerk <github@grimsverk.com>
2026-08-14 07:55:28 +0200

    Update __init__.py

--- src/find_best_mobo/__init__.py
+++ src/find_best_mobo/__init__.py
@@ -1,3 +1,5 @@
-"""Finding Best Mobo by Buildzoid: Searching Buildzoid videos on YT to extract his top pick for an AMD mobo.."""
+"""Finding Best Mobo by Buildzoid:
+Searching Buildzoid videos on YT to extract
+his top pick for an AMD mobo.."""
 
 __version__ = "0.1.0"
