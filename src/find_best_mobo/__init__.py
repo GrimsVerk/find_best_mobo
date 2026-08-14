c334dbaf7789cb1b6efb29ce732fafbc70c61264
GrimsVerk <github@grimsverk.com>
2026-08-14 11:42:16 +0200

    Update from template v0.4.4

--- src/find_best_mobo/__init__.py
+++ src/find_best_mobo/__init__.py
@@ -1,5 +1,12 @@
+<<<<<<< before updating
 """Finding Best Mobo by Buildzoid:
 Searching Buildzoid videos on YT to extract
 his top pick for an AMD mobo.."""
+=======
+"""find-best-mobo.
+
+Searching Buildzoid videos on YT to extract his top pick for an AMD mobo.
+"""
+>>>>>>> after updating
 
 __version__ = "0.1.0"
